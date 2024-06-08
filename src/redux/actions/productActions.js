313b5355b7f872500de30c34c22f577c80d7adb8
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-08 22:25:27 +0200

    redux updated

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const fetchProducts = () => async (dispatch) => {
+  dispatch({ type: "FETCH_PRODUCTS_REQUEST" });
   try {
     const res = await axios.get("http://localhost:5000/api/products");
     dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: res.data });
   } catch (error) {
-    console.error("Error fetching products", error);
     dispatch({ type: "FETCH_PRODUCTS_FAILURE", payload: error.message });
   }
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk"; // Correct named import
 import { composeWithDevTools } from "redux-devtools-extension";
 import productReducer from "./reducers/productReducer";
 
@@ -7,9 +7,11 @@ const rootReducer = combineReducers({
   products: productReducer,
 });
 
+const middleware = [thunk];
+
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
