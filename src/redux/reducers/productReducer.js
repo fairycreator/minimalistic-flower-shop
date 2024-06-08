33bdb620d970302ac90624fbb9240f733deaa5cd
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-08 22:18:23 +0200

    reducer for product updtaed

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,7 +1,28 @@
-const productReducer = (state = [], action) => {
+const initialState = {
+  products: [],
+  loading: false,
+  error: null,
+};
+
+const productReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_PRODUCTS":
-      return action.payload;
+    case "FETCH_PRODUCTS_REQUEST":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "FETCH_PRODUCTS_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
+    case "FETCH_PRODUCTS_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
