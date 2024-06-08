b7cf077103e425648efc6a2c8f109a31caf5501f
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-08 22:18:47 +0200

    store redux updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk"; // Correct import
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import productReducer from "./reducers/productReducer";
-import cartReducer from "./reducers/cartReducer";
-import userReducer from "./reducers/userReducer";
 
 const rootReducer = combineReducers({
   products: productReducer,
-  cart: cartReducer,
-  user: userReducer,
 });
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)) // Correct usage
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
