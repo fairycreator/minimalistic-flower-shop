948c62a7dd01c637a80419e26117f539bdd3d63f
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-08 17:11:39 +0200

    configured Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6WzxrYxzW9Jza68KOMFlcouWeNPp0n_A",
+  authDomain: "flower-shop-aeb86.firebaseapp.com",
+  projectId: "flower-shop-aeb86",
+  storageBucket: "flower-shop-aeb86.appspot.com",
+  messagingSenderId: "499063964194",
+  appId: "1:499063964194:web:c35ee1cd741c972c51007b",
+  measurementId: "G-DYBDMZSBEJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
