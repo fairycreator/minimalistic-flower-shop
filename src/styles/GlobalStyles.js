1660a990d53ef1b53599e5f39083710e3ee37f61
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-09 22:07:12 +0200

    styles updated, added components for HomePage

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,11 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
     font-family: 'Arial', sans-serif;
+    background-color: #f8f8f8;
+    color: #333;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
