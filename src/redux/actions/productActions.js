a040f27bf2a06935d1f7d00b4b633bef304ba50b
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-14 22:51:50 +0200

    product actions

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
 export const fetchProducts = () => async (dispatch) => {
-  dispatch({ type: "FETCH_PRODUCTS_REQUEST" });
   try {
-    const res = await axios.get("http://localhost:5000/api/products");
-    dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: res.data });
+    const response = await axios.get("/api/products");
+    dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: response.data });
   } catch (error) {
-    dispatch({ type: "FETCH_PRODUCTS_FAILURE", payload: error.message });
+    dispatch({ type: "FETCH_PRODUCTS_FAILURE", payload: error.response.data });
+  }
+};
+
+export const fetchProductDetails = (id) => async (dispatch) => {
+  try {
+    const response = await axios.get(`/api/products/${id}`);
+    dispatch({ type: "FETCH_PRODUCT_DETAILS_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({
+      type: "FETCH_PRODUCT_DETAILS_FAILURE",
+      payload: error.response.data,
+    });
   }
 };
