14e1c56fe1fd8515438f675f761c508a888cd20f
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-14 22:51:24 +0200

    updated cart actions

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,3 +1,14 @@
-export const addToCart = (product) => (dispatch) => {
-  dispatch({ type: "ADD_TO_CART", payload: product });
-};
+export const addToCart = (product) => ({
+  type: "ADD_TO_CART",
+  payload: product,
+});
+
+export const removeFromCart = (id) => ({
+  type: "REMOVE_FROM_CART",
+  payload: id,
+});
+
+export const updateCartQuantity = (id, quantity) => ({
+  type: "UPDATE_CART_QUANTITY",
+  payload: { id, quantity },
+});
