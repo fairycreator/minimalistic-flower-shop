ebaa426c7edc3977178c0177af57bab087b06356
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-17 22:55:44 +0200

    .

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,7 +1,17 @@
-const cartReducer = (state = [], action) => {
+const initialState = [];
+
+const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return [...state, action.payload];
+      return [...state, { ...action.payload, quantity: 1 }];
+    case "REMOVE_FROM_CART":
+      return state.filter((item) => item.id !== action.payload);
+    case "UPDATE_CART_QUANTITY":
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, quantity: action.payload.quantity }
+          : item
+      );
     default:
       return state;
   }
