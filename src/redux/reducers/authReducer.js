79ea50d9e948ad9ae76c72f1d8636b933237e4f1
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-18 22:20:22 +0200

    auth reducer updtaed

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,18 +1,22 @@
 const initialState = {
   user: null,
+  loading: false,
   error: null,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOGIN_SUCCESS":
-      return { ...state, user: action.payload, error: null };
-    case "LOGIN_FAILURE":
-      return { ...state, error: action.payload };
+    case "REGISTER_REQUEST":
+    case "LOGIN_REQUEST":
+      return { ...state, loading: true, error: null };
     case "REGISTER_SUCCESS":
-      return { ...state, user: action.payload, error: null };
+    case "LOGIN_SUCCESS":
+      return { ...state, loading: false, user: action.payload };
     case "REGISTER_FAILURE":
-      return { ...state, error: action.payload };
+    case "LOGIN_FAILURE":
+      return { ...state, loading: false, error: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
     default:
       return state;
   }
