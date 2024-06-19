91df22553e10b5566742f7820710dcdf7c032836
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-19 21:35:12 +0200

    navbar updated, added new pages links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../redux/actions/authActions";
 import styled from "styled-components";
 
-const Navbar = () => (
-  <Nav>
-    <Logo>Flower Shop</Logo>
-    <NavLinks>
-      <Link to="/">Home</Link>
-      <Link to="/shop">Shop</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </NavLinks>
-  </Nav>
-);
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
 
-const Nav = styled.nav`
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <NavContainer>
+      <NavLogo to="/">Flower Shop</NavLogo>
+      <NavLinks>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
+        {user ? (
+          <>
+            <NavLink to="/cart">Cart</NavLink>
+            <NavButton onClick={handleLogout}>Logout</NavButton>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </>
+        )}
+      </NavLinks>
+    </NavContainer>
+  );
+};
+
+const NavContainer = styled.nav`
   display: flex;
   justify-content: space-between;
-  padding: 1rem;
-  background-color: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  align-items: center;
+  padding: 1rem 2rem;
+  background: #333;
+  color: white;
 `;
 
-const Logo = styled.div`
+const NavLogo = styled(Link)`
   font-size: 1.5rem;
   font-weight: bold;
+  color: white;
+  text-decoration: none;
 `;
 
 const NavLinks = styled.div`
-  a {
-    margin: 0 1rem;
-    text-decoration: none;
-    color: #333;
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled(Link)`
+  margin-left: 1rem;
+  color: white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const NavButton = styled.button`
+  margin-left: 1rem;
+  background: #555;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  &:hover {
+    background: #777;
   }
 `;
 
