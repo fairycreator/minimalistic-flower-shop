4aac02894c5fe801b2df926bd45d5df15fffc604
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-30 23:41:15 +0200

    product detail page updated

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,62 +1,58 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchProductDetails } from "../redux/actions/productActions";
+import { fetchProductDetail } from "../redux/actions/productActions";
 import styled from "styled-components";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const productDetails = useSelector((state) => state.products.productDetails);
-  const loading = useSelector((state) => state.products.loading);
-  const error = useSelector((state) => state.products.error);
+  const product = useSelector((state) => state.products.productDetail);
 
   useEffect(() => {
-    dispatch(fetchProductDetails(id));
+    dispatch(fetchProductDetail(id));
   }, [dispatch, id]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  if (!product) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <ProductContainer>
-      <ProductImage src={productDetails.image} alt={productDetails.name} />
-      <ProductDetails>
-        <h2>{productDetails.name}</h2>
-        <p>{productDetails.description}</p>
-        <h3>${productDetails.price}</h3>
-        <button>Add to Cart</button>
-      </ProductDetails>
-    </ProductContainer>
+    <ProductDetailContainer>
+      <ProductImage src={product.image} alt={product.name} />
+      <ProductInfo>
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <h3>${product.price}</h3>
+        <AddToCartButton>Add to Cart</AddToCartButton>
+      </ProductInfo>
+    </ProductDetailContainer>
   );
 };
 
-const ProductContainer = styled.div`
+const ProductDetailContainer = styled.div`
   display: flex;
   padding: 2rem;
 `;
 
 const ProductImage = styled.img`
   width: 50%;
-  height: auto;
+  margin-right: 2rem;
 `;
 
-const ProductDetails = styled.div`
-  padding: 2rem;
-  h2,
-  h3 {
-    margin: 0.5rem 0;
-  }
-  button {
-    padding: 0.75rem 1.5rem;
-    background: #333;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    &:hover {
-      background: #555;
-    }
+const ProductInfo = styled.div`
+  flex: 1;
+`;
+
+const AddToCartButton = styled.button`
+  padding: 0.75rem;
+  background: #333;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  &:hover {
+    background: #555;
   }
 `;
 
