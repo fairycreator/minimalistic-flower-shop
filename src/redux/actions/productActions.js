831c8893aaacd1f8e03ef9282060b0cdd34ee5e0
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-07-01 22:54:09 +0200

    productactions updated

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
+export const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
+export const FETCH_PRODUCT_DETAIL_SUCCESS = "FETCH_PRODUCT_DETAIL_SUCCESS";
+export const FETCH_PRODUCTS_FAILURE = "FETCH_PRODUCTS_FAILURE";
+export const FETCH_PRODUCT_DETAIL_FAILURE = "FETCH_PRODUCT_DETAIL_FAILURE";
+
 export const fetchProducts = () => async (dispatch) => {
   try {
     const response = await axios.get("http://localhost:5000/api/products");
-    dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: response.data });
+    dispatch({ type: FETCH_PRODUCTS_SUCCESS, payload: response.data });
   } catch (error) {
-    dispatch({ type: "FETCH_PRODUCTS_FAILURE", payload: error.message });
+    dispatch({ type: FETCH_PRODUCTS_FAILURE, payload: error.message });
   }
 };
 
-export const fetchProductDetails = (id) => async (dispatch) => {
+export const fetchProductDetail = (id) => async (dispatch) => {
   try {
     const response = await axios.get(
       `http://localhost:5000/api/products/${id}`
     );
-    dispatch({ type: "FETCH_PRODUCT_DETAILS_SUCCESS", payload: response.data });
+    dispatch({ type: FETCH_PRODUCT_DETAIL_SUCCESS, payload: response.data });
   } catch (error) {
-    dispatch({ type: "FETCH_PRODUCT_DETAILS_FAILURE", payload: error.message });
+    dispatch({ type: FETCH_PRODUCT_DETAIL_FAILURE, payload: error.message });
   }
 };
