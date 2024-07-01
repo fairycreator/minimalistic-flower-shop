1c144e5e59b3f156759b1b94c053350f47e8e378
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-07-01 22:54:53 +0200

    pruct reducer updated

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,25 @@
+import {
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCT_DETAIL_SUCCESS,
+  FETCH_PRODUCTS_FAILURE,
+  FETCH_PRODUCT_DETAIL_FAILURE,
+} from "../actions/productActions";
+
 const initialState = {
   products: [],
-  productDetails: null,
+  productDetail: null,
+  error: null,
 };
 
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_PRODUCTS_SUCCESS":
-      return { ...state, products: action.payload };
-    case "FETCH_PRODUCTS_FAILURE":
-      return { ...state, products: [] };
-    case "FETCH_PRODUCT_DETAILS_SUCCESS":
-      return { ...state, productDetails: action.payload };
-    case "FETCH_PRODUCT_DETAILS_FAILURE":
-      return { ...state, productDetails: null };
+    case FETCH_PRODUCTS_SUCCESS:
+      return { ...state, products: action.payload, error: null };
+    case FETCH_PRODUCT_DETAIL_SUCCESS:
+      return { ...state, productDetail: action.payload, error: null };
+    case FETCH_PRODUCTS_FAILURE:
+    case FETCH_PRODUCT_DETAIL_FAILURE:
+      return { ...state, error: action.payload };
     default:
       return state;
   }
