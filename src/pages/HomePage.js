f63aa04e2f29f941e0fd11e55268075c551f7232
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-07-06 21:02:02 +0200

    added new sections on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import HeroSection from "../components/HeroSection";
 import FeaturedProducts from "../components/FeaturedProducts";
-import AboutSection from "../components/AboutSection";
-import ContactSection from "../components/ContactSection";
+import CategoriesSection from "../components/CategoriesSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import NewsletterSection from "../components/NewsletterSection";
 import Footer from "../components/Footer";
 
-const HomePage = () => (
-  <>
-    <HeroSection />
-    <FeaturedProducts />
-    <AboutSection />
-    <ContactSection />
-    <Footer />
-  </>
-);
+const HomePage = () => {
+  return (
+    <HomePageContainer>
+      <HeroSection />
+      <FeaturedProducts />
+      <CategoriesSection />
+      <TestimonialsSection />
+      <NewsletterSection />
+      <Footer />
+    </HomePageContainer>
+  );
+};
+
+const HomePageContainer = styled.div`
+  font-family: "Arial", sans-serif;
+`;
 
 export default HomePage;
